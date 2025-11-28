b9208567ef6ca65c184fec818e0a21e5b3cecf4d
Mike Nipkow <mail@mike-nipkow.com>
2025-11-28 20:03:25 +0100

    Mock implementation of devicelist

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import { Box, Grid, GridItem } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
+import DeviceList from "./components/DeviceList"
+import DeviceButton from "./components/DeviceButton"
+import type { ModbusDevice } from "./types/ModbusDevice"
+import { Endian } from "./types/enums/Endian"
+import { FaNetworkWired } from "react-icons/fa6"
+import { FaBook } from "react-icons/fa"
 
 function App() {
+  const mockDevices: ModbusDevice[] = [
+    { filename: "Device1.json", enabled: true, port: 5020, endian: Endian.BigEndian, running: true },
+    { filename: "Device2.json", enabled: false, port: 5021, endian: Endian.BigEndian, running: false },
+    { filename: "Device3.json", enabled: true, port: 5022, endian: Endian.LittleEndian, running: true },
+  ]
+
   return (
     <Grid
       minH="100vh"
@@ -9,14 +21,34 @@ function App() {
       templateColumns="250px 1fr"
       gap={0}
     >
+      {/* Navbar */}
       <GridItem colSpan={2} 
         borderBottom="2px solid" borderColor="brand" 
         boxShadow="0 4px 8px -2px rgba(0, 0, 0, 0.2)">
         <NavBar />
       </GridItem>
+
+      {/* Sidebar */}
       <GridItem borderRight="2px solid" borderColor="brand">
-        Sidebar
+        <DeviceList title="Devices" icon={FaNetworkWired}>
+          {mockDevices.map((device) => (
+            <DeviceButton
+              key={device.filename}
+              device={device}
+            />
+          ))}
+        </DeviceList>
+        <DeviceList title="Templates" icon={FaBook}>
+          {mockDevices.map((device) => (
+            <DeviceButton
+              key={device.filename}
+              device={device}
+            />
+          ))}
+        </DeviceList>
       </GridItem>
+
+      {/* Main Content */}
       <GridItem>
         Main
       </GridItem>
