2d40e79fe070f30a8ab1b0873339b4b4f2bbd93d
Mike Nipkow <mail@mike-nipkow.com>
2025-11-28 18:42:06 +0100

    Add general structure and NavBar component

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,26 @@
+import { Box, Grid, GridItem } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <>
-      Hello World!
-    </>
+    <Grid
+      minH="100vh"
+      templateRows="80px 1fr"
+      templateColumns="250px 1fr"
+      gap={0}
+    >
+      <GridItem colSpan={2} 
+        borderBottom="2px solid" borderColor="brand" 
+        boxShadow="0 4px 8px -2px rgba(0, 0, 0, 0.2)">
+        <NavBar />
+      </GridItem>
+      <GridItem borderRight="2px solid" borderColor="brand">
+        Sidebar
+      </GridItem>
+      <GridItem>
+        Main
+      </GridItem>
+    </Grid>
   )
 }
 
---
+++ packages/frontend/src/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import { HStack, Image, Spacer, Text } from "@chakra-ui/react";
+
+function NavBar() {
+    return (
+        <HStack 
+            margin="0 20px"
+            h="100%" 
+            align="center"
+            position="relative">
+
+            <Image 
+                height="80%"
+                src="/src/assets/logo.png"
+                alt="Modbus Simulator Logo"
+            />
+
+            <Text
+                fontSize="xl"
+                position="absolute"
+                left="50%"
+                transform="translateX(-50%)">
+                Modbus Simulator
+            </Text>
+
+            <Spacer />
+            <Text>v1.0.0</Text>
+        </HStack>
+    );
+}
+
+export default NavBar;
