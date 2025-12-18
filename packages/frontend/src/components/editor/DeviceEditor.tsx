da4314ec0ecbf3da2de8187da46426ad60719dec
Mike Nipkow <mail@mike-nipkow.com>
2025-12-18 15:00:40 +0100

    Change active tab from logs to config when changing from device to template

--- packages/frontend/src/components/editor/DeviceEditor.tsx
+++ packages/frontend/src/components/editor/DeviceEditor.tsx
@@ -7,7 +7,7 @@ import { LuFileClock } from "react-icons/lu";
 import { FaInfo } from "react-icons/fa";
 import { FaGears } from "react-icons/fa6";
 import DeviceLogTable from "./device/DeviceLogTable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   device: ModbusDevice;
@@ -19,11 +19,17 @@ const DeviceEditor = ({ device, onUpdate, onDelete }: Props) => {
   // State to manage the active tab.
   const [activeTab, setActiveTab] = useState<string>("config");
 
+  // Ensure "logs" tab is not active if device is a template.
+  useEffect(() => {
+    if (device.template && activeTab === "logs") setActiveTab("config");
+  }, [device.template, activeTab]);
+
   return (
     <Tabs.Root
       fitted
       variant={"line"}
       defaultValue="config"
+      value={activeTab}
       onValueChange={(e) => setActiveTab(e.value)}
     >
       <Tabs.List>
@@ -35,12 +41,13 @@ const DeviceEditor = ({ device, onUpdate, onDelete }: Props) => {
           <FaGears />
           Modbus Units
         </Tabs.Trigger>
-        {!device.template && (
-          <Tabs.Trigger value="logs">
-            <LuFileClock />
-            Logs
-          </Tabs.Trigger>
-        )}
+        <Tabs.Trigger
+          value="logs"
+          disabled={device.template === true || undefined}
+        >
+          <LuFileClock />
+          Logs
+        </Tabs.Trigger>
       </Tabs.List>
       <Tabs.Content value="config">
         <VStack>
